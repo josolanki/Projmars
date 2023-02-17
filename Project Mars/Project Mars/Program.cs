358f4c2c3b8b18b0cdbb663680282aab700b9050
Jyosolanki <118262106+Jyosolanki@users.noreply.github.com>
2023-02-17 22:06:51 +1000

    Language,Skill,Certification added

--- Project Mars/Project Mars/Program.cs
+++ Project Mars/Project Mars/Program.cs
@@ -45,7 +45,72 @@ IWebElement fluentOption = driver.FindElement(By.XPath("//option[@value='Fluent'
 fluentOption.Click();
 Thread.Sleep(500);
 
+
 IWebElement addLanguageButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[3]/input[1]"));
 addLanguageButton.Click();
 Thread.Sleep(500);
 
+//check if new language is added
+IWebElement languageAdded = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr"));
+languageAdded.Click();
+Thread.Sleep(1000);
+
+//click on Skill
+IWebElement selectSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]"));
+selectSkill.Click();
+Thread.Sleep(1000);
+
+// click on Add new button
+IWebElement addnewButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/thead/tr/th[3]/div"));
+addnewButton.Click();
+Thread.Sleep(1000);
+
+IWebElement skillTextBox = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/div/div[1]/input"));
+skillTextBox.SendKeys("Communication");
+Thread.Sleep(500);
+
+// choose Skill level
+IWebElement skillLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/div/div[2]/select"));
+skillLevel.Click();
+Thread.Sleep(500);
+
+//Select level from dropdown
+IWebElement beginnerOption = driver.FindElement(By.XPath("//option[@value='Beginner' and text() ='Beginner']"));
+beginnerOption.Click();
+Thread.Sleep(500);
+
+// Skill add
+IWebElement addSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/div/span/input[1]"));
+addSkill.Click();
+
+// Click on Certification
+IWebElement clickCertification = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[4]"));
+clickCertification.Click();
+
+// Click on Add new button
+IWebElement addNew = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/table/thead/tr/th[4]/div"));
+addNew.Click();
+
+// Add Certficatons
+IWebElement addCertification = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/div/div[1]/div/input"));
+addCertification.SendKeys("B.A");
+Thread.Sleep(500);
+
+// Add Certification from
+IWebElement certificationFrom = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/div/div[2]/div[1]/input"));
+certificationFrom.SendKeys("Adobe");
+Thread.Sleep(500);
+
+// Add year
+IWebElement addYear = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/div/div[2]/div[2]/select"));
+addYear.Click();
+
+//Add year level
+IWebElement certificationYear = driver.FindElement(By.XPath("//option[@value='2003' and text() ='2003']"));
+certificationYear.Click();
+Thread.Sleep(1000);
+
+// Add new record
+IWebElement newCertificationRecord = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/div/div[3]/input[1]"));
+newCertificationRecord.Click();
+Thread.Sleep(500);
